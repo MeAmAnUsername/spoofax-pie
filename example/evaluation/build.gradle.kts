74d28d8645b19dd4db4a98b18568523d4b090aa6
Ivo Wilms <ivoatinternet@gmail.com>
2021-08-27 17:33:56 +0200

    Set up dependencies for evaluation project

--- example/evaluation/build.gradle.kts
+++ example/evaluation/build.gradle.kts
@@ -1,1 +1,10 @@
-println("In evaluation > build.gradle.kts")
+plugins {
+  id("org.metaborg.gradle.config.java-library")
+}
+
+val pieVersion = "15.0.0"
+
+dependencies {
+  implementation("org.metaborg", "pie.api", pieVersion)
+  implementation("org.metaborg", "pie.runtime", pieVersion)
+}

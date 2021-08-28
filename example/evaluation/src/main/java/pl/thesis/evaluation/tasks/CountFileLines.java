0d197e444c97c73c21e3505696e94c9fbf48f2f7
Ivo Wilms <ivoatinternet@gmail.com>
2021-08-28 04:08:44 +0200

    Fix off-by-one error

--- example/evaluation/src/main/java/pl/thesis/evaluation/tasks/CountFileLines.java
+++ example/evaluation/src/main/java/pl/thesis/evaluation/tasks/CountFileLines.java
@@ -21,13 +21,20 @@ public class CountFileLines implements TaskDef<@NonNull ResourcePath, @NonNull R
     public Result<Integer, @NonNull IOException> exec(@NonNull ExecContext context, @NonNull ResourcePath input) {
         try(InputStream inputStream = context.getReadableResource(input).openRead()) {
             int lineCount = 0;
+            int lastChar = '\n';
             int b;
             while((b = inputStream.read()) != -1) {
+                lastChar = b;
                 if (b == '\n') {
                     lineCount++;
                 }
             }
             assert(inputStream.read() == -1);
+            if (lastChar != '\n') {
+                // a file with x lines only has x-1 newline characters, so add 1
+                // Exception: do not count a trailing newline as a line (do not add 1 if file has a trailing newline)
+                lineCount++;
+            }
             return Result.ofOk(lineCount);
         } catch(IOException e) {
             return Result.ofErr(e);

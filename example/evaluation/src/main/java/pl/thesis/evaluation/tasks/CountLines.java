158444b7c19396be18223726051ae66fbe476b59
Ivo Wilms <ivoatinternet@gmail.com>
2021-08-28 04:14:19 +0200

    Count lines in pie directories as well

--- example/evaluation/src/main/java/pl/thesis/evaluation/tasks/CountLines.java
+++ example/evaluation/src/main/java/pl/thesis/evaluation/tasks/CountLines.java
@@ -3,6 +3,7 @@ package pl.thesis.evaluation.tasks;
 import mb.common.result.Result;
 import mb.pie.api.ExecContext;
 import mb.pie.api.TaskDef;
+import mb.resource.hierarchical.HierarchicalResource;
 import mb.resource.hierarchical.ResourcePath;
 import mb.resource.hierarchical.match.ResourceMatcher;
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -28,16 +29,19 @@ public class CountLines implements TaskDef<@NonNull ResourcePath, @NonNull Resul
     public @NonNull Result<LineCounts, @NonNull Exception> exec(ExecContext context, @NonNull ResourcePath input) {
         try {
             AtomicInteger javaLineCounts = new AtomicInteger();
+            AtomicInteger pieLineCounts = new AtomicInteger();
             List<IOException> errors = new ArrayList<>();
-            context.getHierarchicalResource(input)
-                .walk(ResourceMatcher.ofFileExtension("java"))
+            HierarchicalResource dir = context.getHierarchicalResource(input);
+            dir.walk(ResourceMatcher.ofFileExtension("java"))
                 .map(file -> context.require(countFileLines.createTask(file.getPath())))
                 .forEach(result -> result.ifElse(javaLineCounts::addAndGet, errors::add));
-            // todo: count pie lines as well
+            dir.walk(ResourceMatcher.ofFileExtension("pie"))
+                .map(file -> context.require(countFileLines.createTask(file.getPath())))
+                .forEach(result -> result.ifElse(pieLineCounts::addAndGet, errors::add));
             if (!errors.isEmpty()) {
                 return Result.ofErr(new CompositeException(errors));
             }
-            return Result.ofOk(new LineCounts(javaLineCounts.get(), 0));
+            return Result.ofOk(new LineCounts(javaLineCounts.get(), pieLineCounts.get()));
         } catch(IOException e) {
             return Result.ofErr(e);
         }

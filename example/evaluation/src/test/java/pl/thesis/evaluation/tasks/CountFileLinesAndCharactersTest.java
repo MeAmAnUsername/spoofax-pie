d54dd13bbd0862f5ba44ce1ef544f33630585b23
Ivo Wilms <ivoatinternet@gmail.com>
2021-08-31 03:17:31 +0200

    Switch to TestFactory
    Reads all directories in CASES_DIR and dynamically creates and runs a test for each of them

--- example/evaluation/src/test/java/pl/thesis/evaluation/tasks/CountFileLinesAndCharactersTest.java
+++ example/evaluation/src/test/java/pl/thesis/evaluation/tasks/CountFileLinesAndCharactersTest.java
@@ -2,21 +2,31 @@ package pl.thesis.evaluation.tasks;
 
 import mb.common.result.Result;
 import mb.resource.fs.FSPath;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import pl.thesis.evaluation.Main;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CountFileLinesAndCharactersTest {
-    @Test
-    public void testEmpty() throws Exception {
-        final Path dir = Paths.get("src", "test", "resources", "empty");
-        evaluateTest(dir);
+    public final Path CASES_DIR = Paths.get("src", "test", "resources");
+
+    @TestFactory
+    public Stream<DynamicTest> createTests() throws IOException {
+        return DynamicTest.stream(
+            Files.list(CASES_DIR),
+            dir -> dir.getName(dir.getNameCount() - 1).toString(),
+            this::evaluateTest
+        );
     }
 
     private void evaluateTest(Path dir) throws Exception {

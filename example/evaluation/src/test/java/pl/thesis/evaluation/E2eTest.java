6369f54967e33fe2775e210a1a74e88e1155be56
Ivo Wilms <ivoatinternet@gmail.com>
2021-09-01 15:12:27 +0200

    Move end-to-end tests to own file

---
+++ example/evaluation/src/test/java/pl/thesis/evaluation/E2eTest.java
@@ -0,0 +1,52 @@
+package pl.thesis.evaluation;
+
+import mb.common.result.Result;
+import mb.resource.fs.FSPath;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import pl.thesis.evaluation.tasks.FileCounts;
+import pl.thesis.evaluation.tasks.ProjectEvaluationResult;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class E2eTest {
+    public final Path E2E_PROJECTS_DIR = Paths.get("src", "test", "resources", "e2e-projects");
+
+    @TestFactory
+    public Stream<DynamicTest> createTests() throws IOException {
+        return DynamicTest.stream(
+            Files.list(E2E_PROJECTS_DIR),
+            dir -> dir.getName(dir.getNameCount() - 1).toString(),
+            this::evaluateTest
+        );
+    }
+
+    private void evaluateTest(Path dir) throws Exception {
+        Properties props = new Properties();
+        final Path propFile = dir.resolve("testCase.properties");
+        props.load(new FileInputStream(propFile.toFile()));
+        FileCounts expected = new FileCounts(
+            getIntProperty(props, "lines"),
+            getIntProperty(props, "linesNoLayout"),
+            getIntProperty(props, "characters"),
+            getIntProperty(props, "charactersNoLayout")
+        );
+
+        @SuppressWarnings("NullableProblems") // Cannot find NonNull and error isn't used anyway, so just ignore
+        Result<ProjectEvaluationResult, ?> result = Main.evaluateProject(new FSPath(dir));
+        FileCounts actual = result.unwrap().projectCounts.javaCounts;
+        assertEquals(expected, actual);
+    }
+
+    private static int getIntProperty(Properties properties, String name) {
+        return Integer.parseInt(properties.getProperty(name));
+    }
+}
--- example/evaluation/src/test/java/pl/thesis/evaluation/tasks/CountFileLinesAndCharactersTest.java
+++ example/evaluation/src/test/java/pl/thesis/evaluation/tasks/CountFileLinesAndCharactersTest.java
@@ -1,27 +1,21 @@
 package pl.thesis.evaluation.tasks;
 
-import mb.common.result.Result;
-import mb.resource.fs.FSPath;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
-import pl.thesis.evaluation.Main;
 
 import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Properties;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CountFileLinesAndCharactersTest {
-    public final Path RESOURCES_DIR = Paths.get("src", "test", "resources");
-    public final Path E2E_PROJECTS_DIR = RESOURCES_DIR.resolve("e2e-projects");
-    public final Path UNIT_TESTS_FILE = RESOURCES_DIR.resolve("CountFileLinesAndCharactersTestCases.txt");
+    public final Path UNIT_TESTS_FILE =
+        Paths.get("src", "test", "resources", "CountFileLinesAndCharactersTestCases.txt");
 
     @TestFactory
     public Stream<DynamicTest> readUnitTests() throws IOException {
@@ -46,35 +40,5 @@ public class CountFileLinesAndCharactersTest {
         final InputStream stream = new ByteArrayInputStream(program.getBytes());
         final FileCounts actual = new CountFileLinesAndCharacters().evaluateFile(stream);
         assertEquals(expected, actual);
-    }
-
-    @TestFactory
-    public Stream<DynamicTest> createTests() throws IOException {
-        return DynamicTest.stream(
-            Files.list(E2E_PROJECTS_DIR),
-            dir -> dir.getName(dir.getNameCount() - 1).toString(),
-            this::evaluateTest
-        );
-    }
-
-    private void evaluateTest(Path dir) throws Exception {
-        Properties props = new Properties();
-        final Path propFile = dir.resolve("testCase.properties");
-        props.load(new FileInputStream(propFile.toFile()));
-        FileCounts expected = new FileCounts(
-            getIntProperty(props, "lines"),
-            getIntProperty(props, "linesNoLayout"),
-            getIntProperty(props, "characters"),
-            getIntProperty(props, "charactersNoLayout")
-        );
-
-        @SuppressWarnings("NullableProblems") // Cannot find NonNull and error isn't used anyway, so just ignore
-        Result<ProjectEvaluationResult, ?> result = Main.evaluateProject(new FSPath(dir));
-        FileCounts actual = result.unwrap().projectCounts.javaCounts;
-        assertEquals(expected, actual);
-    }
-
-    private static int getIntProperty(Properties properties, String name) {
-        return Integer.parseInt(properties.getProperty(name));
     }
 }

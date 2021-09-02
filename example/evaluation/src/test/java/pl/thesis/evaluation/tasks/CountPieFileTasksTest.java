a9c4b255ea7ac08741f257e96d0fabd686e25b07
Ivo Wilms <ivoatinternet@gmail.com>
2021-09-02 15:01:37 +0200

    Add tiger file to test, extract common test code

--- example/evaluation/src/test/java/pl/thesis/evaluation/tasks/CountPieFileTasksTest.java
+++ example/evaluation/src/test/java/pl/thesis/evaluation/tasks/CountPieFileTasksTest.java
@@ -2,69 +2,69 @@ package pl.thesis.evaluation.tasks;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CountPieFileTasksTest {
+    private final Path TIGER_PIE_FILE = Paths.get("src", "test", "resources", "tiger.pie");
+
     @Test
     public void noFuncs() {
-        final String program = "module test:count:func:none";
-        final int actual = CountPieFileTasks.countTasks(program);
-        final int expected = 0;
-        assertEquals(expected, actual);
+        assertProgramHasTaskCount(0, "module test:count:func:none");
     }
 
     @Test
     public void foreignFunc() {
-        final String program = "module test:count:func:foreign\n\n" +
+        assertProgramHasTaskCount(0, "module test:count:func:foreign\n\n" +
             "func external() -> unit =\n" +
-            "  foreign External\n";
-        final int actual = CountPieFileTasks.countTasks(program);
-        final int expected = 0;
-        assertEquals(expected, actual);
+            "  foreign External\n");
     }
 
     @Test
     public void dataNamedFunc() {
-        final String program = "module test:count:func:data\n\n" +
+        assertProgramHasTaskCount(0, "module test:count:func:data\n\n" +
             "data func = foreign java Func {\n" +
             "  func get() -> int\n" +
             "  func set(int) -> unit\n" +
-            "}\n";
-        final int actual = CountPieFileTasks.countTasks(program);
-        final int expected = 0;
-        assertEquals(expected, actual);
+            "}\n");
     }
 
     @Test
     public void oneFunc() {
-        final String program = "module test:count:func:one\n\n" +
-            "func somefunc() -> Result<int, _ : Exception> = okResult(0)\n";
-        final int actual = CountPieFileTasks.countTasks(program);
-        final int expected = 1;
-        assertEquals(expected, actual);
+        assertProgramHasTaskCount(1, "module test:count:func:one\n\n" +
+            "func somefunc() -> Result<int, _ : Exception> = okResult(0)\n");
     }
 
     @Test
     public void oneFuncWithBody() {
-        final String program = "module test:count:func:one\n\n" +
+        assertProgramHasTaskCount(1, "module test:count:func:one\n\n" +
             "func somefunc() -> bool = {\n" +
             "  val func = unit;\n" +
             "  val foo: int* = 8;\n" +
-            "}\n";
-        final int actual = CountPieFileTasks.countTasks(program);
-        final int expected = 1;
-        assertEquals(expected, actual);
+            "}\n");
     }
 
     @Test
     public void multipleFuncs() {
-        final String program = "module test:count:func:multiple\n\n" +
+        assertProgramHasTaskCount(3, "module test:count:func:multiple\n\n" +
             "func funcOne() -> Result<int, _ : Exception> = okResult(0)\n" +
             "func funcTwo<E>(e: Optional<E>) -> bool = false\n" +
             "func funcThree()[runtime: StrategoRuntime] -> bool = false\n" +
-            "func funcFour() -> string = foreign java org:example:Foo#getString";
+            "func funcFour() -> string = foreign java org:example:Foo#getString");
+    }
+
+    @Test
+    public void tigerPieFile() throws IOException {
+        assertProgramHasTaskCount(16, new String(Files.readAllBytes(TIGER_PIE_FILE), StandardCharsets.UTF_8));
+    }
+
+    public void assertProgramHasTaskCount(int expectedTaskCount, String program) {
         final int actual = CountPieFileTasks.countTasks(program);
-        final int expected = 3;
-        assertEquals(expected, actual);
+        assertEquals(expectedTaskCount, actual);
     }
 }

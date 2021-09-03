176209ae3f6fc44c22119e21b2a95ff296d3f331
Ivo Wilms <ivoatinternet@gmail.com>
2021-09-03 02:02:00 +0200

    Use Result#ofOkOrCatching

--- example/evaluation/src/main/java/pl/thesis/evaluation/tasks/CountTasks.java
+++ example/evaluation/src/main/java/pl/thesis/evaluation/tasks/CountTasks.java
@@ -27,24 +27,10 @@ public class CountTasks implements TaskDef<@NonNull ResourcePath, @NonNull Resul
 
     @Override
     public @NonNull Result<@NonNull TaskCounts, @NonNull IOException> exec(@NonNull ExecContext context, @NonNull ResourcePath input) {
-        final Result<@NonNull Integer, @NonNull IOException> javaTasks = context.require(countTaskDefs.createTask(input));
-        if (javaTasks.isErr()) {
-            //noinspection ConstantConditions  safe because isErr returned true
-            return Result.ofErr(javaTasks.getErr());
-        }
-        final Result<@NonNull Integer, @NonNull IOException> pieTasks = context.require(countPieTasks.createTask(input));
-        if (pieTasks.isErr()) {
-            //noinspection ConstantConditions  safe because isErr returned true
-            return Result.ofErr(pieTasks.getErr());
-        }
-        final Result<@NonNull Integer, @NonNull IOException> pieTasksWithHelperFunction =
-            context.require(countPieTasksWithHelperFunction.createTask(input));
-        if (pieTasksWithHelperFunction.isErr()) {
-            //noinspection ConstantConditions  safe because isErr returned true
-            return Result.ofErr(pieTasksWithHelperFunction.getErr());
-        }
-
-        //noinspection ConstantConditions  safe because isErr returned false
-        return Result.ofOk(new TaskCounts(javaTasks.get(), pieTasks.get(), pieTasksWithHelperFunction.get()));
+        return Result.ofOkOrCatching(() -> new TaskCounts(
+            context.require(countTaskDefs.createTask(input)).unwrap(),
+            context.require(countPieTasks.createTask(input)).unwrap(),
+            context.require(countPieTasksWithHelperFunction.createTask(input)).unwrap()
+        ), IOException.class);
     }
 }
--- example/evaluation/src/main/java/pl/thesis/evaluation/tasks/EvaluateProject.java
+++ example/evaluation/src/main/java/pl/thesis/evaluation/tasks/EvaluateProject.java
@@ -6,8 +6,6 @@ import mb.pie.api.TaskDef;
 import mb.resource.hierarchical.ResourcePath;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
-import java.io.IOException;
-
 public class EvaluateProject implements TaskDef<@NonNull ResourcePath, @NonNull Result<@NonNull ProjectEvaluationResult, @NonNull Exception>> {
     @NonNull private final CountLinesAndCharacters countLinesAndCharacters;
     @NonNull private final CountTasks countTasks;
@@ -25,19 +23,10 @@ public class EvaluateProject implements TaskDef<@NonNull ResourcePath, @NonNull 
 
     @Override
     @NonNull
-    public Result<@NonNull ProjectEvaluationResult, @NonNull Exception> exec(ExecContext context, @NonNull ResourcePath input) {
-        Result<ProjectCounts, @NonNull Exception> characterCounts = context.require(countLinesAndCharacters.createTask(input));
-        if (characterCounts.isErr()) {
-            //noinspection ConstantConditions  Safe because isErr() returned true
-            return Result.ofErr(characterCounts.getErr());
-        }
-        final Result<@NonNull TaskCounts, @NonNull IOException> taskCounts = context.require(countTasks.createTask(input));
-        if (taskCounts.isErr()) {
-            //noinspection ConstantConditions  Safe because isErr() returned true
-            return Result.ofErr(taskCounts.getErr());
-        }
-
-        //noinspection ConstantConditions  Safe because isErr() returned false
-        return Result.ofOk(new ProjectEvaluationResult(characterCounts.get(), taskCounts.get()));
+    public Result<@NonNull ProjectEvaluationResult, @NonNull Exception> exec(@NonNull ExecContext context, @NonNull ResourcePath input) {
+        return Result.ofOkOrCatching(() -> new ProjectEvaluationResult(
+            context.require(countLinesAndCharacters.createTask(input)).unwrap(),
+            context.require(countTasks.createTask(input)).unwrap()
+        ), Exception.class);
     }
 }
